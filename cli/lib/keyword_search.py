6708b43ea60346a854ba772fad616558b24ec662
RaviRajGhorsai <graviraj344@gmail.com>
2026-03-09 18:44:54 +0545

    added stemming to tokens and also added comments

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,5 +1,8 @@
 from .search_utils import load_movies, load_stop_words
+from nltk.stem import PorterStemmer
 import string
+
+stemmer = PorterStemmer()
 
 
 def clean_text(text):
@@ -8,15 +11,24 @@ def clean_text(text):
     text = text.translate(str.maketrans("", "", string.punctuation))
     return text
 
-
+# Convert search data and query intoword based tokens
 def tokenization(text):
     text = clean_text(text)
     tokens = [tok for tok in text.split() if tok]
 
     tokens = remove_stop_words(tokens)
-    return tokens
 
+    #After removal of stop words we need to stemm tokens, 
+    # like convert running, runs, etc to run. so related searching is easy
+    
+    stemmed_tokens = []
 
+    for token in tokens:
+        stemmed_tokens.append(stemmer.stem(token))
+    
+    return stemmed_tokens
+
+# remove stop words like a, the, etc. that may not have specific meaning to any search
 def remove_stop_words(tokens):
     stop_words = load_stop_words()
 
@@ -47,3 +59,4 @@ def search_command(query, n_results):
         if len(result_movie) == n_results:
             break
     return result_movie
+
