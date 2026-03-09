1078c18b1a2c7ee9bdd0f571f8e02505f974f638
RaviRajGhorsai <graviraj344@gmail.com>
2026-03-09 14:26:49 +0545

    initialized project, loads movies data, apply keyword search(excat match) and lastly apply tokenization

---
+++ cli/lib/keyword_search.py
@@ -0,0 +1,39 @@
+from .search_utils import load_movies
+import string
+
+
+def clean_text(text):
+    text = text.lower()
+
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+def tokenization(text):
+    text = clean_text(text)
+    tokens = [tok for tok in text.split() if tok]
+    return tokens
+
+def has_matching_token(query_tokens, movie_tokens):
+    for q_tok in query_tokens:
+        for m_tok in movie_tokens:
+            if q_tok in m_tok:
+                return True
+    return False
+
+def search_command(query, n_results):
+    data = load_movies()
+    result_movie = []
+
+    query_tokens = tokenization(query)
+    
+
+    for movie in data:
+
+
+        movie_tokens = tokenization(movie["title"])
+
+        if has_matching_token(query_tokens, movie_tokens): 
+            result_movie.append(movie)
+        if len(result_movie) == n_results:
+            break
+    return result_movie
